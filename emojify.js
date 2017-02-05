77f99cda1e9f925ef9019c805061724ea43805e6
Daniel Fischer <d.fischer874@gmail.com>
2017-02-05 08:09:43 -0500

    Changed the popup to accept a string as input, to which it appends a heart emoji and outputs.

---
+++ emojify.js
@@ -0,0 +1,3 @@
+function emojify(string) {
+    return string + ' <img class="emoji" draggable="false" alt="❤️" src="https://twemoji.maxcdn.com/36x36/2764.png">';
+}
--- popup.js
+++ popup.js
@@ -1,3 +1,8 @@
 window.onload = () => {
   document.getElementById("helloEmojiTranslate").addEventListener("click", () => console.log("Hello Emoji Translate!"));
+
+  document.getElementById("emojify").addEventListener("click", () => {
+    var text = document.getElementById("textToBeEmojified").value;
+    document.getElementById("emojifiedText").innerHTML = emojify(text);
+  });
 };
