2c801cf703eafd5db2bf6876a2208c415190ae4d
Daniel Fischer <d.fischer874@gmail.com>
2017-02-05 08:08:10 -0500

    Removed the content script because it was annoying outside of development

--- content.js
+++
@@ -1,3 +0,0 @@
-var img = document.createElement('img');
-img.setAttribute('src', 'https://s-media-cache-ak0.pinimg.com/736x/07/c3/45/07c345d0eca11d0bc97c894751ba1b46.jpg');
-document.getElementsByTagName('body')[0].appendChild(img);
