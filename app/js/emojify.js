eef1318770a2db1be579681341e6bafaa28f0a28
Daniel Fischer <daniel.fischer@cardinalhealth.com>
2017-02-17 13:48:46 -0500

    Created a function, twemojify, that just delegates to twemojify.parse.  Also changed emojify to just call twemojify.

--- app/js/emojify.js
+++ app/js/emojify.js
@@ -1,3 +1,8 @@
 function emojify(string) {
-    return string.replace(/heart/g, '<img class="emoji" draggable="false" alt="❤️" src="https://twemoji.maxcdn.com/36x36/2764.png">')
+//    return string.replace(/heart/g, '<img class="emoji" draggable="false" alt="❤️" src="https://twemoji.maxcdn.com/36x36/2764.png">')
+    return twemoji.parse(string.replace(/heart/g, '\u2764'))
+}
+
+function twemojify(string) {
+    return twemoji.parse(string);
 }
--- test/emoji-test.js
+++ test/emoji-test.js
@@ -1,6 +1,13 @@
 QUnit.test('emojify replaces \'heart\' with heart emoji', function(assert) {
     var text = "I heart you";
-    var emojifiedText = 'I <img class="emoji" draggable="false" alt="❤️" src="https://twemoji.maxcdn.com/36x36/2764.png"> you';
+    var emojifiedText = 'I <img class="emoji" draggable="false" alt="\u2764" src="https://twemoji.maxcdn.com/2/72x72/2764.png"> you';
 
     assert.equal(emojify(text), emojifiedText);
+});
+
+QUnit.test('twemojify replaces unix emojii character codes with emojii images', function(assert) {
+    var text = 'I \u2764 you';
+    var emojifiedText = 'I <img class="emoji" draggable="false" alt="\u2764" src="https://twemoji.maxcdn.com/2/72x72/2764.png"> you';
+
+    assert.equal(twemojify(text), emojifiedText);
 });
