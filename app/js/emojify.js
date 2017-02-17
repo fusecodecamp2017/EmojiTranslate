660080edd2a9b695ede7dd1baff55fdcfb361401
Daniel Fischer <daniel.fischer@cardinalhealth.com>
2017-02-17 15:12:44 -0500

    Implemented twemojifyNode, which accepts an HTMLElement , and for each text node in that element's tree, replaces the word 'heart' with a heart emoji img element.

--- app/js/emojify.js
+++ app/js/emojify.js
@@ -6,3 +6,16 @@ function emojify(string) {
 function twemojify(string) {
     return twemoji.parse(string);
 }
+
+function twemojifyNode(node) {
+    textNodesUnder = function(el) {
+      var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+      while(n=walk.nextNode()) a.push(n);
+      return a;
+    };
+    textNodesUnder(node).forEach(function(textNode) {
+        textNode.textContent = textNode.textContent.replace(/heart/g, '\u2764');
+    });
+    twemoji.parse(node);
+}
+
