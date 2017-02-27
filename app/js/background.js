65a0b02378b4c522e362048a435035bdd2c7a733
Daniel Fischer <daniel.fischer@cardinalhealth.com>
2017-02-26 20:25:34 -0500

    No longer seeding emoji data -- all emoji mappings are now created by the end user at runtime

--- app/js/background.js
+++ app/js/background.js
@@ -1,51 +1,28 @@
 var getEmojis = new Promise(function(resolve, reject) {
-    var emojis = [
-        {
-            word: 'heart',
-            ascii: '\u2764'
-        },
-        {
-            word: 'pencil',
-            ascii: '\u270f'
-        },
-        {
-            word: 'plus',
-            ascii: '\u2795'
-        },
-        {
-            word: 'bread',
-            ascii: '\uD83C\uDF5E'
-        },
-        {
-            word: 'banana',
-            ascii: '\uD83C\uDF4C'
-        },
-    ]
-    chrome.storage.local.set({'emojis': emojis}, function() {
-        chrome.storage.local.get('emojis', function(emojis) {
-            resolve(emojis.emojis);
-        });
+    chrome.storage.local.get('emojis', function(emojis) {
+        resolve(emojis.emojis);
     });
 });
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    console.log(message);
     if( message === 'getEmojis' ) {
         getEmojis.then((emojis) => sendResponse(emojis));
         return true;
     }
 });
 
-var createProperties = {
-    id: "createEmoji",
-    title: "Create Emoji for %s",
-    contexts: ["selection"]
-};
-chrome.contextMenus.create(createProperties, () => {})
+chrome.runtime.onInstalled.addListener(() => {
+    var createProperties = {
+        id: "createEmoji",
+        title: "Create Emoji for %s",
+        contexts: ["selection"]
+    };
+    chrome.contextMenus.create(createProperties, () => {});
+    chrome.storage.local.set({'emojis': []})
+});
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId === "createEmoji") {
-        console.log("Creating emoji for " + info.selectionText);
         var asciiAsString = prompt("Enter the desired Emoji's 4 character hex code below")
         var ascii = String.fromCharCode(parseInt(asciiAsString,16));
         getEmojis = getEmojis.then((emojis) => {
@@ -54,5 +31,6 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
                 ascii: ascii
             }])
         });
+        getEmojis.then((emojis) => chrome.storage.local.set({'emojis': emojis}));
     }
 });
