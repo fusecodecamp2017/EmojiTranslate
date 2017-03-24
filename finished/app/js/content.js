8405421346566fda713a23fc88bb09fe11858453
Daniel Fischer <daniel.fischer@cardinalhealth.com>
2017-03-24 18:25:13 -0400

    Deleted the finished folder

--- finished/app/js/content.js
+++
@@ -1,3 +0,0 @@
-chrome.runtime.sendMessage({request: "getEmojis"}, function(emojis) {
-    twemojifyNode(document.getElementsByTagName('body')[0], emojis);
-});
--- finished/test/ascii-utils-test.js
+++
@@ -1,5 +0,0 @@
-QUnit.test("surrogatePairs returns the correct surrogate pairs given a 5 character ascii character", function(assert) {
-    assert.equal(surrogatePairs('1F193'), '\uD83C\uDD93');
-    assert.equal(surrogatePairs('1F34C'), '\uD83C\uDF4C');
-    assert.equal(surrogatePairs('1F565'), '\uD83D\uDD65');
-});
