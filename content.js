3fa3850b2d46a6d7d62ee28963601319971bb185
Daniel Fischer <daniel.fischer@cardinalhealth.com>
2017-03-25 15:54:46 -0400

    Added content script that displays 'My Name\!' in the browser

---
+++ content.js
@@ -0,0 +1,5 @@
+var body = document.getElementsByTagName("body")[0];
+var myNameHeader = document.createElement("h1");
+var myNameText = document.createTextNode("My Name!");
+myNameHeader.appendChild(myNameText);
+body.appendChild(myNameHeader);
