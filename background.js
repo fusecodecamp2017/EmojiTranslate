9f5ef38632847f04e54054d033c0cbf2c03c5b52
Daniel Fischer <daniel.fischer@cardinalhealth.com>
2017-03-26 19:23:56 -0400

    The content script is now receiving emoji information from the background script

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    sendResponse(new Date().toString());
+    var emojis = [
+        {
+            text: "heart",
+            ascii: '\u2764'
+        },
+        {
+            text: "scissors",
+            ascii: '\u2702'
+        },
+        {
+            text: "lightning",
+            ascii: '\u26A1'
+        }
+    ]
+    sendResponse(emojis);
 });
--- content.js
+++ content.js
@@ -1,10 +1,14 @@
-var textNodesUnder = function(el) {
-  var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
-  while(n=walk.nextNode()) a.push(n);
-  return a;
-};
-var body = document.getElementsByTagName("body")[0];
-textNodesUnder(body).forEach(function(textNode) {
-    var regexp = new RegExp("heart", "gi");
-    textNode.textContent = textNode.textContent.replace(regexp, '\u2764');
+chrome.runtime.sendMessage("", function(emojis) {
+    var textNodesUnder = function(el) {
+      var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+      while(n=walk.nextNode()) a.push(n);
+      return a;
+    };
+    var body = document.getElementsByTagName("body")[0];
+    textNodesUnder(body).forEach(function(textNode) {
+        emojis.forEach(function(emoji) {
+            var regexp = new RegExp(emoji.text, "gi");
+            textNode.textContent = textNode.textContent.replace(regexp, emoji.ascii);
+        });
+    });
 });
