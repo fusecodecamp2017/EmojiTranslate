b94950c4aa1cb9c74569af7f01bfcb635585d0f0
Daniel Fischer <daniel.fischer@cardinalhealth.com>
2017-03-26 11:20:34 -0400

    Replacing the word 'heart' with the heart emoji character

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
-chrome.runtime.sendMessage("", function(response) {
-    var body = document.getElementsByTagName("body")[0];
-    var myNameHeader = document.createElement("h1");
-    var myNameText = document.createTextNode(response);
-    myNameHeader.appendChild(myNameText);
-    body.appendChild(myNameHeader);
+var textNodesUnder = function(el) {
+  var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+  while(n=walk.nextNode()) a.push(n);
+  return a;
+};
+var body = document.getElementsByTagName("body")[0];
+textNodesUnder(body).forEach(function(textNode) {
+    var regexp = new RegExp("heart", "gi");
+    textNode.textContent = textNode.textContent.replace(regexp, '\u2764');
 });
