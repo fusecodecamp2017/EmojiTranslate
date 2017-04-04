b347d0299ee7ca9bd713df9ee82d525aa3a3199e
Daniel Fischer <daniel.fischer@cardinalhealth.com>
2017-04-03 21:08:09 -0400

    Changed background.js to return the emoji mappings stored in local storage instead of hardcoded values

--- background.js
+++ background.js
@@ -1,19 +1,8 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    var emojis = [
-        {
-            text: "heart",
-            ascii: '\u2764'
-        },
-        {
-            text: "scissors",
-            ascii: '\u2702'
-        },
-        {
-            text: "lightning",
-            ascii: '\u26A1'
-        }
-    ]
-    sendResponse(emojis);
+    chrome.storage.local.get("emojis", function(obj) {
+        sendResponse(obj.emojis);
+    });
+    return true;
 });
 
 chrome.runtime.onInstalled.addListener(() => {
