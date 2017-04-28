8a545b6d87dfdeb91f3b479ffd6383607bc51189
Daniel Fischer <daniel.fischer@cardinalhealth.com>
2017-04-28 12:47:59 -0400

    Allowing text to be mapped to 5-character emojis; validating the ascii input is a 4 or 5 alphanumeric character, and alerting an error otherwise

--- background.js
+++ background.js
@@ -17,7 +17,16 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     var asciiAsString = prompt("Enter the desired Emoji's hex code below");
-    var ascii = String.fromCharCode(parseInt(asciiAsString,16));
+    if( !/^[A-Z0-9]{4,5}$/.test(asciiAsString) ) {
+        alert("Invalid input.  Hex code must be 4 or 5 characters, and consist of capital letters and numbers.");
+        return;
+    }
+    var ascii = null;
+    if( asciiAsString.length === 4 ) {
+        ascii = String.fromCharCode(parseInt(asciiAsString,16));
+    } else if( asciiAsString.length === 5 ) {
+        ascii = surrogatePairs(asciiAsString);
+    }
     chrome.storage.local.get("emojis", function(obj) {
         obj.emojis = obj.emojis.concat({
             text: info.selectionText,
@@ -26,3 +35,19 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
         chrome.storage.local.set(obj);
     });
 });
+
+function surrogatePairs(asciiString) {
+    return String.fromCharCode(_getHighCharacter(asciiString)) + String.fromCharCode(_getLowCharacter(asciiString))
+}
+
+function _getHighCharacter(asciiString) {
+    return Math.floor((_hexNumberFromAsciiString(asciiString) - 0x10000) / 0x400) + 0xD800;
+}
+
+function _getLowCharacter(asciiString) {
+    return ((_hexNumberFromAsciiString(asciiString) - 0x10000) % 0x400) + 0xDC00;
+}
+
+function _hexNumberFromAsciiString(asciiString) {
+    return parseInt(asciiString,16);
+}
